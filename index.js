1cff6f03e3306a811d1b5d5ea5af7abd9716d710
hyuntony <ankim90@gmail.com>
2021-06-07 07:48:33 -0400

    completed

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let result = 0;
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      result += sumItems(element);
+    } else {
+      result += element;
+    }
+  }
+  return result;
+};
 
 module.exports = sumItems;
